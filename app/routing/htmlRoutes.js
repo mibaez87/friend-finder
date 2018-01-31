5c7ef6269970a04b60152a2fd41416e24f5bddd4
Michel B <michelb@JB-MBA.local>
2018-01-31 13:36:49 -0500

    html routes set

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
